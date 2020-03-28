681daaa3521783e2c703d7923587709e20b177f4
aymericfloyrac <aymeric.floyrac@polytechnique.edu>
2020-03-28 17:25:23 +0100

    confusion matrix

--- utils/evaluation.py
+++ utils/evaluation.py
@@ -1,7 +1,8 @@
-from sklearn.metrics import accuracy_score,recall_score,precision_score,f1_score
+from sklearn.metrics import accuracy_score,recall_score,precision_score,f1_score,confusion_matrix
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+import seaborn as sns
 
 def evaluate(ytrue,ypred):
 
@@ -25,3 +26,23 @@ def get_binary_metrics(ytrue,ypred):
                           precision_score(ytrue_l,ypred_l,zero_division=0),
                           f1_score(ytrue_l,ypred_l,zero_division=0)  ]
     return results
+
+
+def plot_confusion_matrix(ytrue,ypred):
+    #build confusion matrix
+    plt.figure(figsize=(5,5))
+    labels = np.unique(ytrue)
+    cm = confusion_matrix(ytrue,ypred,labels = labels)
+    cm = np.round(cm/cm.sum(axis=0),3)
+    cm = np.nan_to_num(cm)
+    #plot
+    sns.heatmap(cm, xticklabels = labels, yticklabels = labels, annot = True, fmt='.2f', cmap="Reds", vmin = 0.2)
+    plt.title('Confusion matrix')
+    tick_marks = np.arange(len(labels))
+    plt.xticks(tick_marks, labels, rotation=45)
+    plt.yticks(tick_marks, labels)
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+
+    plt.tight_layout()
+    plt.show()
